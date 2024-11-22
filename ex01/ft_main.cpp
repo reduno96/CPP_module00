857ea5361075c1ba795c8cb851b0deccb1be6772
reduno96 <reduno96@gmail.com>
2024-11-22 08:32:03 +0100

    Refactor ft_main.cpp for improved user experience and functionality

--- ex01/ft_main.cpp
+++ ex01/ft_main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_main.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rel-mora <reduno96@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 08:17:22 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/11/15 11:48:22 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/11/22 00:10:34 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,37 @@
 
 int main ()
 {
-    PhoneBook listOfContact[8];
-
-    for(int i = 0; i < 8; i++)
-    {
-        listOfContact[i].display();
-    }
+    PhoneBook phoneBook;
+        std::string input;
+    int stop;
     
+    stop = 1;
+    std::cout << "Welcome To 80s PhoneBook.\n";
+   while(stop)
+   {
+        std::cout << "Please Enter your Command From the Menu:\n";
+        std::cout << "***********************************************\n";
+        std::cout << "* -ADD: save a new contact.                   *\n";
+        std::cout << "* -SEARCH: display a specific contact.        *\n";
+        std::cout << "* -EXIT: Exit The Program.                    *\n";
+        std::cout << "***********************************************" << std::endl ;
+    
+        if (!(std::getline(std::cin ,input)))
+                std::exit (0);
+        std::cout << std::endl;
+        if (input == "EXIT"){   
+            stop = 0;
+        }
+        else if (input == "ADD" ){
+            phoneBook.addContact();
+        }
+        else if (input == "SEARCH" ){
+            phoneBook.searchContact();
+        }
+        else{
+            std::cout << "!!!!!Invalid input." << std::endl << std::endl ;
+        }
+   }
+   std::cout << "Bye!!" << std::endl;
+   return 0;
 }
