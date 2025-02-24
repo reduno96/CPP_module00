fcf9240fc0243e14d8de20da23cc29d0b576aa2b
reduno96 <reduno96@gmail.com>
2025-02-24 02:12:07 +0000

    first implementation

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,29 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 01:09:16 by rel-mora          #+#    #+#             */
+/*   Updated: 2025/02/24 02:04:03 by rel-mora         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include <string>
+#include <iostream>
+
+class Harl
+{
+private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+   
+public:
+    Harl();
+    ~Harl();
+    void complain( std::string level );
+    
+};
