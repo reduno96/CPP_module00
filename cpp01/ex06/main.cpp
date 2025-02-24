bd6281eef1e0fbf4fc594d447f9914304115fe2a
reduno96 <reduno96@gmail.com>
2025-02-24 02:38:27 +0000

    second implementation

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,13 +6,17 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 02:09:01 by rel-mora          #+#    #+#             */
-/*   Updated: 2025/02/24 02:09:07 by rel-mora         ###   ########.fr       */
+/*   Updated: 2025/02/24 02:36:58 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "Harl.hpp"
 
-int main(){
-    Harl ff = Harl();
-    ff.complain("DEBUG");
+int main(int ac, char **av){
+    if (ac == 2){
+        Harl ff = Harl();
+        ff.complain(av[1]);
+    }
+    else
+        std::cout << "Error: your're enter invalid argument!!\n";
 }
