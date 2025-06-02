3d283a5fa72a86a5eede319982927fe0a3e4e00d
reduno96 <reduno96@gmail.com>
2025-06-02 14:34:28 +0100

    add:

---
+++ cpp04/ex00/Cat.hpp
@@ -0,0 +1,16 @@
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+#include <string>
+#include <iostream>
+
+class Cat : public Animal
+{
+private:
+public:
+    Cat();
+    ~Cat();
+};
+
+#endif
---
+++ cpp04/ex00/Dog.hpp
@@ -0,0 +1,16 @@
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+#include <string>
+#include <iostream>
+
+class Dog : public Animal
+{
+private:
+public:
+    Dog();
+    ~Dog();
+};
+
+#endif
